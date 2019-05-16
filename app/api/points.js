74efeaa4b05069ef2114c4dfe266bd4f952e5b64
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-05-16 15:36:40 +0100

    created additional test's

--- app/api/points.js
+++ app/api/points.js
@@ -25,7 +25,39 @@ const Points = {
         return Boom.notFound('No point with this ID');
       }
     }
+  },
+
+  create: {
+    auth: false,
+    handler: async function(request, h) {
+      const newPoint = new Point(request.payload);
+      const point = await newPoint.save();
+      if(point) {
+        return h.response(point).code(201);
+      }
+      return Boom.badImplementation('error creating point');
+    }
+  },
+
+  deleteAll: {
+    auth: false,
+    handler: async function(request, h) {
+      await Point.remove({});
+      return { success: true};
+    }
+  },
+
+  deleteOne:{
+    auth: false,
+    handler: async function(request, h) {
+      const point = await Point.remove({_id: request.params.id});
+      if(point) {
+        return { succes:true };
+      }
+      return Boom.notFound('id not found');
+    }
   }
+
 };
 
 module.exports = Points;
--- routesapi.js
+++ routesapi.js
@@ -1,6 +1,9 @@
-const Points = require('./app/api/points');
+const PointsApi = require('./app/api/points');
 
 module.exports = [
-  { method: 'GET', path: '/api/points', config: Points.find },
-  { method: 'GET', path: '/api/points/{id}', config: Points.findOne }
+  { method: 'GET', path: '/api/points', config: PointsApi.find },
+  { method: 'GET', path: '/api/points/{id}', config: PointsApi.findOne },
+  { method: 'POST', path: '/api/candidates', config: PointsApi.create },
+  { method: 'DELETE', path: '/api/candidates/{id}', config: PointsApi.deleteOne },
+  { method: 'DELETE', path: '/api/candidates', config: PointsApi.deleteAll },
 ];
